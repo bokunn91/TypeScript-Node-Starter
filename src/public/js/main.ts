8e2aae94db448b7e14954ae162c1dcec3b0aaf44
김보근 <bokunn91@gmail.com>
2018-01-31 22:02:01 +0900

    Append card using javascript.

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -1,4 +1,25 @@
-$(document).ready(function() {
-  // Place JavaScript code here...
+// Self invoking function.
+// It is wrapped by (). It runs immediately.
+(function() {
 
-});
+  // "strict" operating context.
+  "use strict";
+
+  const app = {
+    isLoading: true,
+    visibleCards: {},
+    spinner: document.querySelector(".loader"),
+    cardTemplate: document.querySelector(".cardTemplate"),
+    container: document.querySelector(".main"),
+    addDialog: document.querySelector(".dialog-container"),
+  };
+
+  let card = app.cardTemplate.cloneNode(true);
+  card.attributes.removeNamedItem("hidden");
+  app.container.appendChild(card);
+
+  card = app.cardTemplate.cloneNode(true);
+  card.attributes.removeNamedItem("hidden");
+  app.container.appendChild(card);
+
+})();
